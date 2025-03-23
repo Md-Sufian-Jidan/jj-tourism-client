42845c1f775e9fc7c3f330556dd61a295bae725d
Md-Sufian-Jidan <jidanjiyaj03@gmail.com>
2025-03-23 23:38:39 +0600

    my list page added

--- src/Pages/MyList/MyList.jsx
+++ src/Pages/MyList/MyList.jsx
@@ -1,12 +1,69 @@
+import { useContext, useEffect, useState } from 'react';
 import { Helmet } from 'react-helmet';
+import { AuthContext } from '../../Routes/AuthProvider';
 
 const MyList = () => {
+
+    const { user } = useContext(AuthContext);
+    const [places, setPlaces] = useState([]);
+
+    useEffect(() => {
+        fetch(`${import.meta.env.VITE_LOCALHOST_API}/my-list/${user?.email}`)
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                setPlaces(data);
+            });
+    }, []);
+
     return (
         <div>
             <Helmet>
                 <title>Jj Tourism || My List</title>
             </Helmet>
-            My List
+
+            <div className="overflow-x-auto">
+                <table className="table">
+                    {/* head */}
+                    <thead>
+                        <tr>
+                            <th>No</th>
+                            <th>Name</th>
+                            <th>Country</th>
+                            <th>Add By</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {/* row 1 */}
+                        {
+                            places?.map((place, idx) => <tr key={place?._id}>
+                                <th>{idx + 1}</th>
+                                <td>
+                                    <div className="flex items-center gap-3">
+                                        <div className="avatar">
+                                            <div className="mask mask-squircle h-12 w-12">
+                                                <img
+                                                    src={place?.image}
+                                                    alt="Avatar Tailwind CSS Component" />
+                                            </div>
+                                        </div>
+                                        <div>
+                                            <div className="font-bold">{place?.tourists_spot_name}</div>
+                                            <div className="text-sm opacity-50">{place?.location}</div>
+                                        </div>
+                                    </div>
+                                </td>
+                                <td>{place?.Country}</td>
+                                <td>{user?.email}</td>
+                                <th>
+                                    <button className="btn bg-[#3F7D58]">Update</button>
+                                </th>
+                            </tr>)
+                        }
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
